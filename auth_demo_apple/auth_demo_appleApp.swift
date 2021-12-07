7008a703fe6b4a4ac0093181c1b4638b6343b301
Edward Phan <76992905+edphan@users.noreply.github.com>
2021-12-07 15:50:39 -0800

    Initial Commit

---
+++ auth_demo_apple/auth_demo_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  auth_demo_appleApp.swift
+//  auth_demo_apple
+//
+//  Created by Edward Phan on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct auth_demo_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
